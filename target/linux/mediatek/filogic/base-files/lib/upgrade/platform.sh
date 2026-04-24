950c4182bdb0ef62dff1135049c6058ef3a1cf5b
Morton Li <Morton.L@Outlook.com>
2026-04-25 01:13:11 +0800

    跟进最新平台源码

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -1,64 +1,28 @@
 REQUIRE_IMAGE_METADATA=1
+RAMFS_COPY_BIN='fitblk'
 
 platform_do_upgrade() {
-	local board=$(board_name)
-
-	case "$board" in
-	cmcc,rax3000m)
-		case "$(cmdline_get_var root)" in
-		/dev/mmc*)
-			CI_KERNPART="production"
-			emmc_do_upgrade "$1"
-			;;
-		*)
-			CI_KERNPART="fit"
-			nand_do_upgrade "$1"
-			;;
-		esac
-		;;
-	*)
-		nand_do_upgrade "$1"
-		;;
-	esac
+	fit_do_upgrade "$1"
 }
 
 PART_NAME=firmware
 
 platform_check_image() {
-	local board=$(board_name)
 	local magic="$(get_magic_long "$1")"
 
 	[ "$#" -gt 1 ] && return 1
 
-	case "$board" in
-	cmcc,rax3000m)
-		[ "$magic" != "d00dfeed" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	*)
-		nand_do_platform_check "$board" "$1"
-		return $?
-		;;
-	esac
-
+	[ "$magic" != "d00dfeed" ] && {
+		echo "Invalid image type."
+		return 1
+	}
 	return 0
 }
 
 platform_copy_config() {
-	case "$(board_name)" in
-	cmcc,rax3000m)
-		case "$(cmdline_get_var root)" in
-		/dev/mmc*)
-			emmc_copy_config
-			;;
-		esac
-		;;
-	esac
+	if [ "$CI_METHOD" = "emmc" ]; then
+		emmc_copy_config
+	fi
 }
 
-platform_pre_upgrade() {
-	local board=$(board_name)
-}
+platform_pre_upgrade() {}
